5b4217c622022cc67110ad44e0d034298a1f8bd5
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-07 10:21:33 -0400

    Basic task adding funtion added as well as due date.  A basic outline of the page spacing is in place

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,36 @@
-console.log('This is cool!');
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+
+//Task Object Constructor
+
+var NewTask = function(options) {
+  var args = options || {};
+  this.task = options.task;
+  // this.date = options.date;
+  this.status = 'Incomplete';
+};
+
+//Task Creation
+
+var storage = [];
+
+$('#addTask').on('submit',function(event) {
+  event.preventDefault();
+  var taskText = $('#taskText').val();
+  var taskDate = $('#taskDate').val();
+  // var date = date.format('MM/DD/YY');
+  var taskInstance = new NewTask({task: taskText});
+  storage.push(taskInstance);
+  $('#tasks').append('<li class="incomplete task"><span class="taskHead">' + taskText + '</span><span class="dateHead">' + taskDate + '</span></li>');
+  this.reset();
+});
+
+// //Toggle
+
+$('#tasks').on('click', 'li', function() {
+  event.preventDefault();
+  $(this).toggleClass('complete');
+  var tTask = $(this).text();
+  var taskToEdit = _.find(storage, {task: tTask});
+  taskToEdit.status = 'Complete';
+});
+

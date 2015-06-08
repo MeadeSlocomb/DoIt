821c610ff2358dd6d193f15870b60fa49b155f0e
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-08 19:47:53 -0400

    Fixed delete functionality

--- app/js/main.js
+++ app/js/main.js
@@ -20,7 +20,8 @@ $('#addTask').on('submit',function(event) {
   var taskDate = $('#taskDate').val();
   var taskInstance = new NewTask({task: taskText, date: taskDate});
   storage.push(taskInstance);
-  $('#tasks').append('<li class="incomplete task"><div class="taskText"><span>' + taskText + '</span></div><div class="taskDate"><p>Due: ' + taskDate + '</p></div></li><div class="delete"><i class="fa fa-trash-o"></i></div>');
+  $('#tasks').append('<li class="incomplete task"><div class="taskText"><span>' + taskText + '</span></div><div class="taskDate"><p>Due: ' + taskDate + '</p></div><div class="delete"><i class="fa fa-trash-o"></i></div></li>');
+
   this.reset();
 });
 
@@ -31,14 +32,19 @@ $('#tasks').on('click', 'li', function() {
   $(this).find('span').toggleClass('complete');
   $(this).find('p').toggleClass('complete');
   var tTask = $(this).find('span').text();
+  console.log(tTask);
   var taskToEdit = _.find(storage, {task: tTask});
   taskToEdit.complete = !taskToEdit.complete;
 });
 
 //Delete
 
-$('.delete').on('click', function(){
-  $(this).prev('li').addClass('deleted');
+$('#tasks').on('click', '.delete', function(){
+  $(this).closest('li').addClass('deleted');
+  var tTask = $(this).closest('li').find('span').text();
+  console.log(tTask);
+  var taskToEdit = _.find(storage, {task: tTask});
+  taskToEdit.deleted = !taskToEdit.deleted;
 });
 
 

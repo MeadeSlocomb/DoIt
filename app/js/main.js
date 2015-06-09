195b35ef2603372bae13b6749e534a4d73fdd7dd
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-08 22:32:23 -0400

    Added prototype function to edit date string to a more easily read format

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,10 @@ var NewTask = function(options) {
   this.task = options.task;
   this.date = options.date;
   this.complete = false;
-  this.deleted = false;
+};
+
+NewTask.prototype.formattedDate = function() {
+    return this.date.substr(5, 9);
 };
 
 //Task Creation
@@ -19,9 +22,9 @@ $('#addTask').on('submit',function(event) {
   var taskText = $('#taskText').val();
   var taskDate = $('#taskDate').val();
   var taskInstance = new NewTask({task: taskText, date: taskDate});
+  var taskFormDate = taskInstance.formattedDate();
   storage.push(taskInstance);
-  $('#tasks').append('<li class="incomplete task"><div class="taskText"><span>' + taskText + '</span></div><div class="taskDate"><p>Due: ' + taskDate + '</p></div><div class="delete"><i class="fa fa-trash-o"></i></div></li>');
-
+  $('#tasks').append('<li class="incomplete task"><div class="taskText"><span>' + taskText + '</span></div><div class="taskDate"><p>Due: ' + taskFormDate + '</p></div><div class="delete"><i class="fa fa-trash-o"></i></div></li>');
   this.reset();
 });
 
@@ -32,8 +35,8 @@ $('#tasks').on('click', 'li', function() {
   $(this).find('span').toggleClass('complete');
   $(this).find('p').toggleClass('complete');
   var tTask = $(this).find('span').text();
-  console.log(tTask);
   var taskToEdit = _.find(storage, {task: tTask});
+  console.dir(taskToEdit);
   taskToEdit.complete = !taskToEdit.complete;
 });
 
@@ -42,9 +45,6 @@ $('#tasks').on('click', 'li', function() {
 $('#tasks').on('click', '.delete', function(){
   $(this).closest('li').addClass('deleted');
   var tTask = $(this).closest('li').find('span').text();
-  console.log(tTask);
   var taskToEdit = _.find(storage, {task: tTask});
   taskToEdit.deleted = !taskToEdit.deleted;
 });
-
-
